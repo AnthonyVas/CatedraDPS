2b8924fe6eb58f4e30801fc1efa09d0ee985be17
AnthonyVas <vasquezanth@gmail.com>
2021-10-14 20:24:57 -0600

    Primera version

---
+++ appSeguimientoMedico/src/screens/login/login.js
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import { View, Text } from 'react-native';
+
+const login = () =>{
+    return(
+ <View>
+     <Text>
+         This is america
+     </Text>
+ </View>
+ );
+}
+
+export default login;
