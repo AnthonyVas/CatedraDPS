00cd73938e292efbdb92bc0ff365884f7cb851a3
AnthonyVas <vasquezanth@gmail.com>
2021-10-15 18:43:05 -0600

    Splash v1

--- appSeguimientoMedico/App.js
+++ appSeguimientoMedico/App.js
@@ -1,7 +1,13 @@
 import React from "react";
 import Login  from "./src/screens/login/login";
+import SplashScreen from 'react-native-splash-screen'
+
 
 export default function App() {
+  //Hide Splash screen on app load.
+  React.useEffect(() => {
+    SplashScreen.hide();
+  });
   return(
     <Login></Login>
   );
--- appSeguimientoMedico/src/screens/login/login.js
+++ appSeguimientoMedico/src/screens/login/login.js
@@ -1,14 +1,63 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, Image } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { Input } from 'react-native-elements';
+import { TextInput } from 'react-native-paper';
+import { red100 } from 'react-native-paper/lib/typescript/styles/colors';
 
-const login = () =>{
-    return(
- <View>
-     <Text>
-         This is america
-     </Text>
- </View>
- );
+
+// console.log(Icon);
+
+const login = () => {
+    const [text, setText] = React.useState('');
+    return (
+
+        <View>
+            <View style={styles.imageContainer} >
+                <Image
+                style={styles.image}
+                    source={require('../../imgs/logosplash.png')}
+                >
+
+                </Image>
+            </View>
+            <View style={styles.container}>
+                <TextInput
+                    label="Usuario/Correo"
+                    value={text}
+                    onChangeText={text => setText(text)}
+                />
+                <TextInput
+                    label="Contraseña"
+                    value={text}
+                    onChangeText={text => setText(text)}
+                    style={styles.contra}
+                />
+            </View>
+           
+        </View>
+    );
 }
 
+const styles = StyleSheet.create({
+    container: {
+        margin: 15
+    },
+    contra:{
+        marginTop:15
+    },
+    image:{      
+        width: '60%',
+        height: '90%',
+        // backgroundColor: 'yellow',
+        
+    },
+    imageContainer:{
+        marginTop: 50,
+        height: '40%',
+        // backgroundColor: 'red',
+        justifyContent: 'center',
+        alignItems: 'center',
+    }
+})
 export default login;
