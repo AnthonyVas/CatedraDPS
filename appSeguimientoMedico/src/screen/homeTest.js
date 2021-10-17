e5405e0692da7a914ae4e618fb497b9741b18416
WILLIAMS AMAYA <whistemalo@gmail.com>
2021-10-17 17:45:12 -0600

    cambios para el merge con la rama de pruebas

--- appSeguimientoMedico/src/screen/homeTest.js
+++ appSeguimientoMedico/src/screen/homeTest.js
@@ -2,17 +2,17 @@ import * as React from 'react';
 import { Button, View } from 'react-native';
 import { createDrawerNavigator, DrawerContent } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
-import mainTabScreen from './src/navigation/mainTabScreen';
+import mainTabScreen from './mainTabScreen';
 
 //Components
-import CustomDrawerContent from './src/navigation/CustomDrawerContent'
-import dentisList from './src/navigation/dentisList';
-import myAppointments from './src/navigation/myAppointments';
-import bookAppointment from './src/navigation/bookAppointment';
+import CustomDrawerContent from './CustomDrawerContent'
+import dentisList from './dentisList';
+import myAppointments from './myAppointments';
+import bookAppointment from './bookAppointment';
 
 const Drawer = createDrawerNavigator();
 
-export default function App() {
+export default function homeTest() {
   return (
     <NavigationContainer>
       <Drawer.Navigator 
